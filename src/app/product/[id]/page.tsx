c703f54360c9e65333979b6a52fb29fa2c9a020d
Advait <76993204+777advait@users.noreply.github.com>
2024-07-08 16:54:38 +0530

    add variants preview

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,9 +2,36 @@ import Container from "@/components/Container";
 import { getProductById } from "@/lib/get-products";
 import Image from "next/image";
 import VariantForm from "@/components/Product/VariantForm";
-
+import {
+  ArrowLeftToLineIcon,
+  ShoppingBagIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+function VariantCard(props: { title: string; image: string; price: string }) {
+  return (
+    <div className="rounded-md border border-gray-100 bg-gray-400 bg-opacity-10 bg-clip-padding p-4 backdrop-blur-md backdrop-filter">
+      <div className="space-y-4">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={props.image}
+            alt={props.title}
+            width={200}
+            height={200}
+            className="rounded-md"
+          />
+        </div>
+        <div className="space-y-2">
+          <h1 className="font-semibold">{props.title}</h1>
+          <p className="text-sm text-muted-foreground">{props.price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const product: {
@@ -27,7 +54,14 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <Container>
-      <div className="space-y-8">
+      <Link
+        href="/"
+        className={`${buttonVariants({ variant: "link" })} !px-0 pb-8 text-xs !text-muted-foreground`}
+      >
+        <ArrowLeftToLineIcon className="mr-1.5 h-3.5 w-3.5" />
+        Back to catalog
+      </Link>
+      <div className="space-y-12">
         <div className="flex flex-col items-center justify-center md:flex-row md:items-start md:justify-center md:gap-12">
           <div className="md:w-1/2">
             <Image
@@ -52,16 +86,30 @@ export default async function Page({ params }: { params: { id: string } }) {
               variant_count={product.product.variant_count}
               currency={product.product.currency}
             />
-            <div className="flex items-center gap-2 px-2">
-              <Button size="sm">Buy Now</Button>
+            <div className="flex items-center gap-4 px-2">
+              <Button size="sm">
+                <ShoppingBagIcon className="mr-1.5 h-3.5 w-3.5" />
+                Buy Now
+              </Button>
               <Button size="sm" variant="outline">
+                <ShoppingCartIcon className="mr-1.5 h-3.5 w-3.5" />
                 Add to Cart
               </Button>
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="space-y-4">
           <h1 className="text-lg font-semibold">Variants Preview</h1>
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-4">
+            {product.variants.map((variant) => (
+              <VariantCard
+                key={variant.id}
+                title={variant.name}
+                image={variant.image}
+                price={`${product.product.currency} ${variant.price}`}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Container>
